ed7755d8cdfb3c92df58d6019c345b1170278b84
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-11-17 10:41:07 -0500

    added prediction step

--- examples/example-1.py
+++ examples/example-1.py
@@ -6,10 +6,20 @@ train = pd.DataFrame([['1', '5', 19],
                       ['2', '43', 33],
                       ['3', '20', 55],
                       ['4', '10', 20]],
-                     columns=['user', 'item', 'age'])
+                     columns = ['user', 'item', 'age'])
 
-X = csr_matrix(pd.get_dummies(train, prefix=['user', 'item']).values)
-y = np.repeat(1.0, X.shape[0])
+X_train = csr_matrix(pd.get_dummies(train, prefix = ['user', 'item']).values)
+y_train = np.repeat(1.0, X_train.shape[0])
 
 fm = pylibfm.FM()
-fm.fit(X,y)
+fm.fit(X_train, y_train)
+
+test = pd.DataFrame([['1', '5', 19],
+                      ['2', '43', 33],
+                      ['3', '20', 55],
+                      ['4', '10', 20]],
+                     columns = ['user', 'item', 'age'])
+
+X_test = csr_matrix(pd.get_dummies(test, prefix = ['user', 'item']).values)
+
+fm.predict(X_test)

c21fa1cbe41bf1aaa91e05bc602c74d859f4d313
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-11-17 11:21:15 -0500

    formatting

--- examples/example-1.py
+++ examples/example-1.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import string
 
+
 n = 9000
 user = [str(u) for u in xrange(n)]
 item = [i for i in np.random.choice(list(string.ascii_lowercase), size=n)]
@@ -11,7 +12,7 @@ age = [a for a in np.random.randint(low=18, high=100, size=n)]
 
 train = pd.DataFrame({'user': user, 'item': item, 'age': age})
 
-X_train = csr_matrix(pd.get_dummies(train, prefix = ['user', 'item']).values)
+X_train = csr_matrix(pd.get_dummies(train, prefix=['user', 'item']).values)
 y_train = np.repeat(1.0, X_train.shape[0])
 
 fm = pylibfm.FM()
@@ -24,6 +25,6 @@ age = [a for a in np.random.randint(low=18, high=100, size=n)]
 
 test = pd.DataFrame({'user': user, 'item': item, 'age': age})
 
-X_test = csr_matrix(pd.get_dummies(test, prefix = ['user', 'item']).values)
+X_test = csr_matrix(pd.get_dummies(test, prefix=['user', 'item']).values)
 
 fm.predict(X_test)

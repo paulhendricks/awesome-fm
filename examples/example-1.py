e0ccbf55e67cf6fd97d25699c47b7db32775641e
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-11-17 11:10:48 -0500

    added random sampling

--- examples/example-1.py
+++ examples/example-1.py
@@ -2,11 +2,14 @@ import pylibfm
 from scipy.sparse import csr_matrix
 import numpy as np
 import pandas as pd
-train = pd.DataFrame([['1', '5', 19],
-                      ['2', '43', 33],
-                      ['3', '20', 55],
-                      ['4', '10', 20]],
-                     columns = ['user', 'item', 'age'])
+import string
+
+n = 9000
+user = [str(u) for u in xrange(n)]
+item = [i for i in np.random.choice(list(string.ascii_lowercase), size=n)]
+age = [a for a in np.random.randint(low=18, high=100, size=n)]
+
+train = pd.DataFrame({'user': user, 'item': item, 'age': age})
 
 X_train = csr_matrix(pd.get_dummies(train, prefix = ['user', 'item']).values)
 y_train = np.repeat(1.0, X_train.shape[0])
@@ -14,11 +17,12 @@ y_train = np.repeat(1.0, X_train.shape[0])
 fm = pylibfm.FM()
 fm.fit(X_train, y_train)
 
-test = pd.DataFrame([['1', '5', 19],
-                      ['2', '43', 33],
-                      ['3', '20', 55],
-                      ['4', '10', 20]],
-                     columns = ['user', 'item', 'age'])
+n = 1000
+user = [str(u) for u in xrange(n)]
+item = [i for i in np.random.choice(list(string.ascii_lowercase), size=n)]
+age = [a for a in np.random.randint(low=18, high=100, size=n)]
+
+test = pd.DataFrame({'user': user, 'item': item, 'age': age})
 
 X_test = csr_matrix(pd.get_dummies(test, prefix = ['user', 'item']).values)
 
